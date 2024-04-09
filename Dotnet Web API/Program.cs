e264f30c1c4d87a639cabe4e050095bf72b3ac43
Alex Johnson <johnsonav1992@gmail.com>
2024-04-09 06:16:14 -0500

    format

--- Dotnet Web API/Program.cs
+++ Dotnet Web API/Program.cs
@@ -1,11 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddRouting(options => options.LowercaseUrls = true);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 var app = builder.Build();
 
