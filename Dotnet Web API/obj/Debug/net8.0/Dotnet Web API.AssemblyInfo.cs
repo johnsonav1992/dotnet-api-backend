932d8198bc699ef3daed7eafd6af8eb8a8044c9a
Alex Johnson <johnsonav1992@gmail.com>
2024-04-09 06:38:04 -0500

    database update

--- Dotnet Web API/obj/Debug/net8.0/Dotnet Web API.AssemblyInfo.cs
+++ Dotnet Web API/obj/Debug/net8.0/Dotnet Web API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dotnet Web API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c04e4fc9fae73e5176788de0e9e4d5d821ccc073")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0772277b6900cc8c406f934e98131baa6f27b0c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dotnet Web API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dotnet Web API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
